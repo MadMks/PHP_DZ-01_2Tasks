68e7aa28b89ca95aa227b19af670028f093b64e7
Makc <mks59k@gmail.com>
2018-11-18 23:03:27 +0200

    Создание файлов.

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
 ?>
+
+<ul class="nav nav-pills">
+    <li role="presentation">
+        <a href="task1/task1.php">Task 1</a>
+    </li>
+    <li role="presentation">
+        <a href="task2/task2.php">Task 2</a>
+    </li>
+<ul>
